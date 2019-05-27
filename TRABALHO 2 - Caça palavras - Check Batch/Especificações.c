7a98cf393253b3cf2be90980e6e941654fcd7a5d
Mayk Bezerra <38228656+mayktu@users.noreply.github.com>
2019-05-26 23:13:37 -0300

    Add files via upload

---
+++ TRABALHO 2 - Caça palavras - Check Batch/Especificações.c
@@ -0,0 +1,32 @@
+
+/*Você deverá criar um arquivo batch que acionará uma versão modificada do seu trabalho 1 que deverá receber como parâmetros 3 nomes de arquivos: 
+a) um contendo uma matriz de caracteres;
+b) outro contendo uma lista de palavras; 
+c) um contendo o resultado esparado (sim se a matriz contem todas as palavras da lista de palavras e não, caso contrário).
+ 
+O objetivo do programa é indicar se a matriz contem todas as palavras da lista de palavras ou não.
+Portanto, o resultado sempre deverá ser sim ou não.
+
+O arquivo batch chamará o programa modificado 15 vezes e no final será possível avaliar o programa originalmente submetido no trabalho 1 atribuindo-lhe uma nota.
+
+A nota do trabalho 1 será composta por:
+
+         a) Indentação:    1.25 pontos;
+
+         b) Comentários: 1.25 pontos
+
+         c) 0.5 ponto para cada acerto nos casos de teste (até 15 pontos)
+
+     O trabalho 2 deverá ser enviado em um único arquivo contendo:
+
+        a) A avaliação do trabalho 1;
+
+        b) Uma análise crítica sobre o que poderia ser modificado no trabalho 1 para ele receber 10.0;
+
+        c) O código do programa batch; e
+
+        d) O código do programa fonte do Trabalho 2 devidamente documentado e indentado (você não vai querer perder pontos nessa altura do campeonato, vai?!).
+
+Os casos de teste serão disponibilizados em 10/06/2018. A entrega deverá ser no dia 15/06/2018 até as 18h.*/
+
+
